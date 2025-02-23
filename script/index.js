385fb47c8ccf194316b83232f9fba48a296f9019
yoona <an0oynihs@naver.com>
2025-02-24 03:40:38 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,3 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const section1 = document.getElementById('section1');
+    const scrollIcon = document.querySelector('.scroll-icon a');
+    const footer = document.getElementById('footer');
+
+    section1.addEventListener('scroll', function() {
+    const sectionBottom = section1.scrollTop + section1.clientHeight;
+    const footerTop = footer.offsetTop;
+
+    if (sectionBottom >= footerTop) {
+        // footer가 보이면 아이콘을 up으로 변경
+        scrollIcon.classList.remove('down');
+        scrollIcon.classList.add('up');
+        scrollIcon.querySelector('img').src = './images/icon/scroll-up.png';
+    } else {
+        // 그렇지 않으면 down 아이콘 유지
+        scrollIcon.classList.remove('up');
+        scrollIcon.classList.add('down');
+        scrollIcon.querySelector('img').src = './images/icon/scroll-down.png';
+    }
+    });
+});
+
 // section2 배너 슬라이드
 const nowSlide = document.querySelector('.now');
 const totalSlide = document.querySelector('.total');
